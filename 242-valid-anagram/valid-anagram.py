97601fd1be70abed895a61ad3de56cfc70fc0129
Uma Chinnam <123286714+uma-1510@users.noreply.github.com>
2026-08-31 13:37:56 -0400

    Time: 13 ms (51.35%) | Memory: 19.5 MB (46.00%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        count = [0] * 26
+
+        for c in s:
+            count[ord(c) - ord('a')] +=1
+
+        for c in t:
+            count[ord(c) - ord('a')] -=1
+
+        return all(x==0 for x in count)
+
+        
